800866facbe71bfbeee0ec33934713966fd9b8f3
nicola <notsecurity@gmail.com>
2014-11-14 15:21:28 +0000

    adding engineering works

--- scraper/parser/timetable_scrapingf.py
+++ scraper/parser/timetable_scrapingf.py
@@ -9,7 +9,7 @@ def extractDesc(string):
     m = re.search('<br\/>(?:.*)delays(?:\ between)?\ (.*)\ (?:to|and)\ (.*)\ due(?:.*)<br\/>', string)
     if m:
         return (m.group(1), m.group(2))
-    return ''
+    return ('', '')
 
 
 def getLineName(line_row):
@@ -29,8 +29,6 @@ def getDescription(cell):
 
 
 def extract(date):
-    print ("DOIN'", date)
-
     txt = open('../raw/' + date + '.html')
     response = txt.read()
 
@@ -47,34 +45,31 @@ def extract(date):
     for line_row, time_row in zip(lines_table.findAll('tr'), timetable.findAll('tr')):
         lines += [(
                 cell.findAll('a')[0].string,
-                getDescription(cell),
+                description[0],
+                description[1],
                 24 - (i+1)*0.25,
                 getLineName(line_row),
                 date
             )
-            for i, cell in enumerate(filter(isValidCell, time_row.findAll('td'))) if notGS(cell)]
+            for i, cell in enumerate(filter(isValidCell, time_row.findAll('td')))
+            for description in [getDescription(cell)]
+            if notGS(cell)]
 
     txt.close()
     return lines
 
 def toCSV(array, name):
-    # for x in array:
-    #     asStr = x.__str__()
-    #     print(asStr[1:len(asStr)-1])
+    for x in array:
+        for y in x:
+            asStr = y.__str__()
+            print(asStr[1:len(asStr)-1])
 
-    with open(name+'.csv','w') as out:
-        csv_out=csv.writer(out)
-        for row in array:
-            csv_out.writerow(row)
-
-def execute(array, name):
-
-        toCSV(extract(array), name)
+    # with open(name+'.csv','w') as out:
+    #     csv_out=csv.writer(out)
+    #     for row in array:
+    #         csv_out.writerow(row)
 
 
-def chunks(l, n):
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
 
 def main():
     # for x in extract("../raw/2014-01-01.html"):
@@ -84,14 +79,15 @@ def main():
     # print(extract("2014-01-01"))
     # for x in extract("../raw/2014-01-01.html")[0]:
     #     for y in x: print (y)
-    onlyfiles = reversed([f.replace('.html', '') for f in listdir('../raw') ])
+    onlyfiles = [f.replace('.html', '') for f in listdir('../raw') ]
     l = []
     for date in onlyfiles:
         try:
-            data = [extract(date)]
-            toCSV(data, date)
+            l += [extract(date)]
         except:
-            print ("Failed for date", date)
+            pass
+
+    toCSV(l, 'name')
 
 
 
