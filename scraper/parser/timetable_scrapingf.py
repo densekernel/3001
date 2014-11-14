f93d548e6752138e029f98b370020bbc2ea0aa83
nicola <notsecurity@gmail.com>
2014-11-14 11:44:53 +0000

    adding stations

--- scraper/parser/timetable_scrapingf.py
+++ scraper/parser/timetable_scrapingf.py
@@ -1,21 +1,54 @@
 import bs4
-import pymysql
-import urllib.request
+import re
 
-response = urllib.request.urlopen('http://tubestatus.net/timeline')
 
-soup = bs4.BeautifulSoup(response)
+def extractDesc(string):
+    m = re.search('<br\/>(?:.*)delays(?:\ between)?\ (.*)\ (?:to|and)\ (.*)\ due(?:.*)<br\/>', string)
+    if m:
+        return (m.group(1), m.group(2))
+    return ''
 
-lines_table = soup.findAll('div', {'class': 'history_left'})[0] \
-                  .findAll('table', {'class': 'history'})[0]
 
-timetable = soup.findAll('div', {'class': 'history_right'})[0] \
-                .findAll('div', {'class': 'scrollbig'})[0] \
-                .findAll('table')[0]
+def getLineName(line_row):
+    return line_row.findAll('td')[0].findAll('font')[0].findAll('b')[0].string
 
-for line_row, time_row in zip(lines_table.findAll('tr'), timetable.findAll('tr')):
-  print(line_row.findAll('td')[0].findAll('font')[0].findAll('b')[0].string, ':',
-        [cell.findAll('a')[0].string
-         for cell in time_row.findAll('td') if cell.findAll('a') != []])
 
-#print(timetable)
+def isValidCell(cell):
+    return cell.findAll('a') != []
+
+
+def getDescription(cell):
+    return extractDesc(cell.findAll('div')[0].__str__())
+
+
+def extract(filename):
+
+    txt = open(filename)
+    response = txt.read()
+
+    soup = bs4.BeautifulSoup(response)
+
+    lines_table = soup.findAll('div', {'class': 'history_left'})[0] \
+                    .findAll('table', {'class': 'history'})[0]
+
+    timetable = soup.findAll('div', {'class': 'history_right'})[0] \
+                  .findAll('div', {'class': 'scrollbig'})[0] \
+                  .findAll('table')[0]
+
+    lines = []
+    for line_row, time_row in zip(lines_table.findAll('tr'), timetable.findAll('tr')):
+        lines += [(getLineName(line_row),
+            [(cell.findAll('a')[0].string, getDescription(cell))
+                for cell in time_row.findAll('td') if isValidCell(cell)])]
+
+    txt.close()
+    return lines
+
+
+def main():
+    for x in extract("../raw/2014-01-01.html"):
+        for y in x[1]:
+            if (y[0] == 'SD'):
+                print(y)
+
+main()
