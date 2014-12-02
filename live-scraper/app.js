bbc7b1aad3a4fea47e7db38466a181130e93cb83
Navid Hallajian <navidhg@gmail.com>
2014-12-02 16:22:59 +0000

    Added correct nodejs file for API

--- live-scraper/app.js
+++ live-scraper/app.js
@@ -1,23 +1,58 @@
-var pg = require('pg')
+var pg = require('pg');
+var express = require('express');
+var app = express();
 
-var conString = "postgres//navidhg:bikepass@localhost/bikehire"
-
+// Connect to postgres
+var conString = "postgres://postgres:bikepass@localhost/bikedata";
 var client = new pg.Client(conString);
 
-client.connect(function(err) {
-  if(err) {
-    return console.error('Could not connect', err);
-  }
-  client.query('SELECT COUNT(*) FROM static2', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
+// API routes
+app.get('/station', function(req, res) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('Could not connect to postgres.', err);
     }
-    console.log(JSON.parse(result.rows[0]));
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
+    console.log('Querying...');
+    client.query('SELECT * FROM static', function(err, result) {
+      if (err) {
+        return console.error('Error running query', err);
+      }
+      res.type('text/plain');
+      res.json(result);
+    });
   });
 });
 
+app.get('/station/:id', function(req, res) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('Could not connect to postgres.', err);
+    }
+    console.log('Querying...');
+    client.query('SELECT * FROM static WHERE id = $1', [req.params.id], function(err, result) {
+      if (err) {
+        return console.error('Error running query', err);
+      }
+      res.type('text/plain');
+      res.json(result.rows);
+    });
+  });
+});
 
-//appendix b 3.3.1
+app.get('/station/:id/data', function(req, res) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('Could not connect to postgres.', err);
+    }
+    console.log('Querying...');
+    client.query('SELECT * FROM live WHERE stationid = $1', [req.params.id], function(err, result) {
+      if (err) {
+        return console.error('Error running query', err);
+      }
+      res.type('text/plain');
+      res.json(result.rows);
+    });
+  });
+});
 
+app.listen(5000);
