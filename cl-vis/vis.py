f9670e2ca3ee27c301f9f57f0fd75511693cf084
Navid Hallajian <navidhg@gmail.com>
2014-12-05 12:24:46 +0000

    Changed directory for python vis

---
+++ cl-vis/vis.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python
+
+import json
+import sys
+import urllib2
+import matplotlib.pyplot as plt
+
+stationID = sys.argv[1]
+urlconn = urllib2.urlopen("http://178.62.32.221:5000/station/" + stationID + "/data")
+data = json.load(urlconn)
+
+times = []
+bikes = []
+
+for d in data:
+	times.append(int(d['sampletime']))
+	bikes.append(int(d['nbbikes']))
+
+plt.plot(times, bikes)
+plt.show()
+
+urlconn.close()
